4c1c21756db7ab7277b7721dcceb40e0aa85be74
Lucas Llano <llano.lucas.04@gmail.com>
2023-06-19 22:32:27 -0300

    variable innecesaria eliminada

--- js/main.js
+++ js/main.js
@@ -1,4 +1,2 @@
 const selectorProductos = document.getElementById("selector_productos");
 const carritoProductos = document.getElementById("productos_carrito");
-const subtotal = document.getElementById("subtotal");
-let arrayProductos = [];

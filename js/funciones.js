1bc4a82054b2deffb4222a12197288b755f5ad5d
Lucas Llano <llano.lucas.04@gmail.com>
2023-06-19 22:31:08 -0300

    funciones optimizadas

--- js/funciones.js
+++ js/funciones.js
@@ -68,10 +68,10 @@ function sacarDelCarrito(id) {
 }
 
 function restarDelCarrito(id) {
-  let carrito = cargarCarrito();
+  const carrito = cargarCarrito();
 
   if (estaEnElCarrito(id)) {
-    let pos = carrito.findIndex((item) => item.id === id);
+    const pos = carrito.findIndex((item) => item.id === id);
     if (carrito[pos].cantidad > 1) {
       carrito[pos].cantidad -= 1;
       carrito[pos].precio = carrito[pos].cantidad * 600;
@@ -100,61 +100,119 @@ function carritoLength() {
   return carrito.length;
 }
 
-// sacarDelCarrito(0);
-
 function renderCarrito() {
-  let productos = cargarCarrito();
+  const productos = cargarCarrito();
   let contenido = "";
 
   if (carritoLength() > 0) {
-    productos.forEach((producto) => {
-      contenido += `<ul>
-      <li>${producto.sabor}</li>
-      <li>x${producto.cantidad}u</li>
-      <li>$${producto.precio}</li>
-    </ul>`;
-    });
-    carritoProductos.innerHTML = contenido;
+    contenido = productos
+      .map(
+        (producto) => `<ul>
+        <li>${producto.sabor}</li>
+        <li>x${producto.cantidad}u</li>
+        <li>$${producto.precio}</li>
+      </ul>`
+      )
+      .join("");
   } else {
     contenido = `<ul class="m-auto mb-1">No hay productos en el carrito...</ul>`;
-    carritoProductos.innerHTML = contenido;
   }
+
+  carritoProductos.innerHTML = contenido;
   renderSubtotal();
 }
 
 function botonRender() {
-  const carro = document.getElementById("carro");
-  if (carritoLength() > 0) {
-    if (carro.childElementCount < 2) {
-      let span = document.createElement("span");
-      span.setAttribute("id", "boton-carrito");
-      carro.appendChild(span);
-      const boton = document.getElementById("boton-carrito");
-      boton.innerHTML = carritoLength();
-    } else {
-      const botoncito = document.getElementById("boton-carrito");
-      botoncito.innerHTML = carritoLength();
+  const carritoLengthValue = carritoLength();
+  if (carritoLengthValue > 0) {
+    let boton = document.getElementById("boton-carrito");
+    if (!boton) {
+      const carro = document.getElementById("carro");
+      boton = document.createElement("span");
+      boton.setAttribute("id", "boton-carrito");
+      carro.appendChild(boton);
     }
+    boton.textContent = carritoLengthValue;
   }
 }
 
 function renderSubtotal() {
   const carrito = cargarCarrito();
+  const precioEnvio = carrito.length > 0 ? 400 : 0;
   const sumaPrecios = carrito.reduce(
     (acumulador, elemento) => acumulador + elemento.precio,
     0
   );
-  const subtotal = document.getElementById("subtotal");
-  subtotal.innerHTML = `$${sumaPrecios}`;
+  const totalPrecio = sumaPrecios + precioEnvio;
 
-  const envio = document.getElementById("envio");
-  const precioEnvio = cargarCarrito().length > 0 ? 400 : 0;
-  envio.innerHTML = `$${precioEnvio}`;
-
-  const total = document.getElementById("total");
-  totalPrecio = sumaPrecios + precioEnvio;
-  total.innerHTML = `$${sumaPrecios + precioEnvio}`;
+  document.getElementById("subtotal").textContent = `$${sumaPrecios}`;
+  document.getElementById("envio").textContent = `$${precioEnvio}`;
+  document.getElementById("total").textContent = `$${totalPrecio}`;
 }
 
+function renderSelector() {
+  cargarProducto().forEach((element) => {
+    if (element.tipo == "dulces") {
+      selectorProductos.innerHTML += `<li class="nuevo">
+        <div class="carrito__productos__descripcion">
+          <img src=${element.imagen} alt="talita" />
+          <div>
+            <h4>${element.sabor}</h4>
+            <p>${element.descripcion}</p>
+          </div>
+        </div>
+        <div class="carrito__productos__botonera">
+          <span class="carrito__productos__botonera__menos" id="menos${element.sabor.replaceAll(
+            " ",
+            ""
+          )}"
+          onclick="restarDelCarrito(${element.id})">-</span
+          >
+          <span class="carrito_prodcutos__botonera_num" id="cant${element.sabor.replaceAll(
+            " ",
+            ""
+          )}"
+            >${cantidadSelector(element.id)}</span
+          >
+          <span class="carrito__productos__botonera__mas" id="mas${element.sabor.replaceAll(
+            " ",
+            ""
+          )}"
+           onclick="agregarAlCarrito(${element.id})">+</span
+          >
+        </div>
+      </li>`;
+    } else {
+      selectorProductos.innerHTML += `<li>
+          <div class="carrito__productos__descripcion">
+            <img src=${element.imagen} alt="talita" />
+            <div>
+              <h4>${element.sabor}</h4>
+              <p>${element.descripcion}</p>
+            </div>
+          </div>
+          <div class="carrito__productos__botonera">
+            <span class="carrito__productos__botonera__menos" id="menos${
+              element.sabor
+            }"
+            onclick="restarDelCarrito(${element.id})">-</span
+            >
+            <span class="carrito_prodcutos__botonera_num" id="cant${
+              element.sabor
+            }"
+              >${cantidadSelector(element.id)}</span
+            >
+            <span class="carrito__productos__botonera__mas" id="mas${
+              element.sabor
+            }"
+            onclick="agregarAlCarrito(${element.id})">+</span
+            >
+          </div>
+        </li>`;
+    }
+  });
+}
+
+renderSelector();
 renderCarrito();
 botonRender();
